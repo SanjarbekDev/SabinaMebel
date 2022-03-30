6dd8e615757ad5e477f0d2483611f48b02cbf1db
Sanjarbek{Dev} <sanjarbeksodiqov0302@gmail.com>
2022-03-30 20:08:10 +0500

    config.py
    config.py

--- data/config.py
+++ data/config.py
@@ -22,9 +22,9 @@ BOT_TOKEN = str(os.environ.get('BOT_TOKEN'))
 ADMINS = ['155460463',"1672039210"] # adminlar ro'yxati
 IP = str(os.environ.get('ip'))
 
-# DB_USER = str(os.environ.get("DB_USER"))
-# DB_PASS = str(os.environ.get("DB_PASS"))
-# DB_NAME = str(os.environ.get("DB_NAME"))
-# DB_HOST = str(os.environ.get("DB_HOST"))
+DB_USER = str(os.environ.get("DB_USER"))
+DB_PASS = str(os.environ.get("DB_PASS"))
+DB_NAME = str(os.environ.get("DB_NAME"))
+DB_HOST = str(os.environ.get("DB_HOST"))
 
 DATABASE_URL=str(os.environ.get("DATABASE_URL"))
